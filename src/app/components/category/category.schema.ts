8b64adb8068f986c20211f581c388d2ed7d6d910
Anibal <anibal78e@gmail.com>
2022-05-12 18:04:06 -0400

    user added, petitions tested

--- src/app/components/category/category.schema.ts
+++ src/app/components/category/category.schema.ts
@@ -2,9 +2,12 @@ import { Schema, model, Document } from "mongoose";
 import { Category } from "../../models/category.model";
 
 const definition: Partial<Record<keyof Category, any>> = {
-  name: { type: String, required: true, lowercase: true, trim: true }
+  name: { type: String, required: true, lowercase: true, trim: true },
+  products: [{ type: Schema.Types.ObjectId, ref: 'Product', required: true, trim: true, autopopulate: true }],
+  categories: [{ type: Schema.Types.ObjectId, ref: 'Category', required: false, trim: true, autopopulate: true }],
+
 };
 
 const schema: Schema<Category> = new Schema(definition, { timestamps: true });
-
+schema.plugin(require('mongoose-autopopulate'));
 export default model<Category & Document>('Category', schema, 'category');
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,11 +2,13 @@ import { Express } from "express";
 import feature from "./feature";
 import category from "./category";
 import product from "./product";
+import user from "./user";
 
 const components: Express[] = [
     feature,
     category,
-    product
+    product,
+    user
 ];
 
 export default components
