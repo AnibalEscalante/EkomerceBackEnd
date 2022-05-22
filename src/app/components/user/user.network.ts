3a1e197aaa335e7767f7995190b23149dfc2c941
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2022-05-22 16:27:41 -0400

    update getUser and updateUser

--- src/app/components/user/user.network.ts
+++ src/app/components/user/user.network.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Router } from "express";
-/* import { Auth } from "../../models/auth.model"; */
+import { Auth } from "../../models/auth.model";
 import { User } from "../../models/user.model";
 import response from "../../modules/reponse.module";
 import controller from "./user.controller";
@@ -30,7 +30,32 @@ router.get('/:id', async (req: Request, res: Response) => {
   }
 });
 
+router.patch('/:id', async (req: Request, res: Response) => {
+  const user: Partial<User & Auth> = req.body;
+  const id: string = req.params['id'];
 
+  try {
+    const result: any | null = await controller.updateUser(id, user);
+    response.success(req, res, result);
+  }
+  catch (error) {
+    console.error(error);
+    response.error(req, res, 'Invalid information', 500);
+  }
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+  const id: string = req.params['id'];
+
+  try {
+    const result: User | null = await controller.deleteUser(id);
+    response.success(req, res, result);
+  }
+  catch (error) {
+    console.error(error);
+    response.error(req, res, 'Invalid information', 500);
+  }
+});
 
 /* 
 router.patch('/:id', async (req: Request, res: Response) => {
